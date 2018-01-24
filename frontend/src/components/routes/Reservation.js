8d509e4dc002789af168ee79914128711771abf0
KyeJune Hong <hohoyaing@gmail.com>
2018-01-24 20:59:22 +0900

    (FE) parcel제거

--- frontend/src/components/routes/Reservation.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Reversition = () => (
-    <div>
-        예약
-    </div>
-);
-
-export default Reversition;
--- frontend/src/scripts/connect.js
+++
@@ -1,3 +0,0 @@
-document.addEventListener('deviceready', function(){
-    alert('APP START CONNECT');
-}, false);
