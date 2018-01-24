a3854622eb6112c287411b17dfd94c97a35eb53b
uruya* <uru@grotesq.com>
2018-01-24 16:08:06 +0900

    (FE) boost-on 추가. CardItem 스타일 적용.

--- frontend/src/components/ui/CardItem.js
+++ frontend/src/components/ui/CardItem.js
@@ -1,147 +1,155 @@
-import React, {Component} from 'react';
-import {BrowserHistory, withRouter} from 'react-router';
-import {Avatar, Button, Card, CardActions, CardText, CardTitle, Media, MediaOverlay} from 'react-md';
-import {Link} from 'react-router-dom';
+import React, { Component } from 'react';
+import { BrowserHistory, withRouter } from 'react-router';
+import { Avatar, Button, Card, CardActions, CardText, CardTitle, Media, MediaOverlay } from 'react-md';
+import { Link } from 'react-router-dom';
 
 class CardItem extends Component {
 
-    // 피드, 뉴스 작성자 정보
-    cardAuthorInfo = () => {
-        if (this.props.cardData.type !== 'event') {
-            return (
-                <CardTitle
-                    // 작성자 이름
-                    title={this.props.cardData.author.name}
-                    // 작성자 프로필 이미지
-                    avatar={<Avatar src={this.props.cardData.author.avatar} role="presentation"/>}
-                    // 작성 시간
-                    subtitle={this.props.cardData.created_at}
-                />
-            );
-        }
-        else {
-            return '';
-        }
-    };
+	// 피드, 뉴스 작성자 정보
+	cardAuthorInfo = ()=> {
+		if( this.props.cardData.type !== 'event' ) {
+			return (
+				<CardTitle
+					// 작성자 이름
+					title={this.props.cardData.author.name}
+					// 작성자 프로필 이미지
+					avatar={<Avatar src={this.props.cardData.author.avatar} role="presentation"/>}
+					// 작성 시간
+					subtitle={this.props.cardData.created_at}
+				/>
+			);
+		}
+		else {
+			return '';
+		}
+	};
 
-    // 이벤트 썸네일, 제목
-    cardEventTitle = () => {
-        if (this.props.cardData.type === "event") {
-            return (
-                <div>
-                    <Media>
-                        <img src={this.props.cardData.thumbnail}/>
-                        <MediaOverlay>
-                            <CardTitle
-                                // 이벤트 제목
-                                title={this.props.cardData.event_title}
-                            />
-                        </MediaOverlay>
-                    </Media>
-                </div>
-            );
-        }
-        else {
-            return '';
-        }
-    };
+	// 이벤트 썸네일, 제목
+	cardEventTitle = ()=> {
+		if( this.props.cardData.type === "event" ) {
+			return (
+				<div>
+					<Media>
+						<img src={this.props.cardData.thumbnail}/>
+						<MediaOverlay>
+							<CardTitle
+								// 이벤트 제목
+								title={this.props.cardData.event_title}
+							/>
+						</MediaOverlay>
+					</Media>
+				</div>
+			);
+		}
+		else {
+			return '';
+		}
+	};
 
-    // 이벤트 장소, 일정
-    cardEventContent = () => {
-        if (this.props.cardData.type === "event") {
-            return (
-                <div>
-                    {/*이벤트 장소*/}
-                    <div>{this.props.cardData.event_schedule}</div>
-                    {/*이벤트 일정*/}
-                    <div>{this.props.cardData.event_location}</div>
-                </div>
-            );
-        }
-        else {
-            return '';
-        }
-    };
+	// 이벤트 장소, 일정
+	cardEventContent = ()=> {
+		if( this.props.cardData.type === "event" ) {
+			return (
+				<div className="cl-card-item__event-content">
+					{/*이벤트 장소*/}
+					<h3>{this.props.cardData.event_schedule}</h3>
+					{/*이벤트 일정*/}
+					<p>{this.props.cardData.event_location}</p>
+				</div>
+			);
+		}
+		else {
+			return '';
+		}
+	};
 
-    // 피드, 뉴스 컨텐츠
-    cardTextContent = () => {
-        if (this.props.cardData.type !== "event") {
-            return (
-                <p>
-                    {this.props.cardData.content}
-                </p>
-            );
-        }
-        else {
-            return '';
-        }
-    };
+	// 피드, 뉴스 컨텐츠
+	cardTextContent = ()=> {
+		if( this.props.cardData.type !== "event" ) {
+			return (
+				<p>
+					{this.props.cardData.content}
+				</p>
+			);
+		}
+		else {
+			return '';
+		}
+	};
 
-    // Share
-    cardShare = () => {
-        if (this.props.cardData.type !== "feed") {
-            return (
-                <Button flat>Share</Button>
-            );
-        }
-        else {
-            return '';
-        }
-    };
+	// Share
+	cardShare = ()=> {
+		if( this.props.cardData.type !== "feed" ) {
+			return (
+				<Button flat className="cl-card-item__button"><span>Share</span></Button>
+			);
+		}
+		else {
+			return '';
+		}
+	};
 
-    // RSVP
-    cardRsvp = () => {
-        if (this.props.cardData.type === 'event') {
-            return (
-                <Button flat>+rsvp</Button>
-            );
-        }
-        else {
-            return '';
-        }
-    };
+	// RSVP
+	cardRsvp = ()=> {
+		if( this.props.cardData.type === 'event' ) {
+			return (
+				<Button flat className="cl-card-item__rsvp"></Button>
+			);
+		}
+		else {
+			return '';
+		}
+	};
 
-    render() {
-
-
-        if (this.props.cardData) {
-            return (
-                <Card style={{maxWidth: "600px"}} className="md-block-centered">
-                    {/* 작성자 정보 - 피드, 뉴스 */}
-                    {this.cardAuthorInfo()}
-
-                    <Link to={ this.props.list + '/view/' + 0 }>
-                        {/* 이벤트 썸네일, 제목 */}
-                        {this.cardEventTitle()}
-
-                        <CardText>
-                            {/* 이벤트 장소, 일정 */}
-                            {this.cardEventContent()}
-
-                            {/*내용*/}
-                            {this.cardTextContent()}
-                        </CardText>
-                    </Link>
-
-                    <CardActions>
-                        {/* Like */}
-                        <Button flat>Like <i>{this.props.cardData.like_count}</i></Button>
-
-                        {/* Share */}
-                        {this.cardShare()}
-
-                        {/* RSVP */}
-                        {this.cardRsvp()}
-                    </CardActions>
+	render () {
 
 
-                </Card>
-            )
-        }
-        else {
-            return '';
-        }
-    }
+		if( this.props.cardData ) {
+			return (
+				<div className="cl-card-item">
+					<Card style={{ maxWidth: "600px" }} className="md-block-centered">
+						{/* 작성자 정보 - 피드, 뉴스 */}
+						{this.cardAuthorInfo()}
+
+						<Link to={this.props.list + '/view/' + 0}>
+							{/* 이벤트 썸네일, 제목 */}
+							{this.cardEventTitle()}
+
+							<CardText>
+								{/* 이벤트 장소, 일정 */}
+								{this.cardEventContent()}
+
+								{/*내용*/}
+								{this.cardTextContent()}
+							</CardText>
+						</Link>
+
+						<hr/>
+
+						<CardActions>
+							<div className="h-group">
+								<div className="col">
+									{/* Like */}
+									<Button flat className="cl-card-item__button"><span>Like</span>
+										<i>{this.props.cardData.like_count}</i></Button>
+
+									{/* Share */}
+									{this.cardShare()}
+								</div>
+								<div className="col right">
+									{/* RSVP */}
+									{this.cardRsvp()}
+								</div>
+							</div>
+						</CardActions>
+					</Card>
+				</div>
+			)
+		}
+		else {
+			return '';
+		}
+	}
 }
 
-export default withRouter(CardItem);
+export default withRouter( CardItem );
