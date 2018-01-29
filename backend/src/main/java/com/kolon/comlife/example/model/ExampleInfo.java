ddc34f2b20e20e5eaa141654b29ba48d14a7398a
Young Kim <ykim.hello@gmail.com>
2018-01-29 19:26:13 +0900

    Example Code: Exception handling in Controller

--- backend/src/main/java/com/kolon/comlife/example/model/ExampleInfo.java
+++ backend/src/main/java/com/kolon/comlife/example/model/ExampleInfo.java
@@ -1,6 +1,7 @@
 package com.kolon.comlife.example.model;
 
 public class ExampleInfo {
+    private int id;
     private String name;
 
     public ExampleInfo() {
@@ -9,6 +10,15 @@ public class ExampleInfo {
     public ExampleInfo(String name) {
         this.name = name;
     }
+
+    public ExampleInfo(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public void setId(int id) { this.id = id; }
+
+    public int getId() { return this.id; }
 
     public void setName(String name) {
         this.name = name;
