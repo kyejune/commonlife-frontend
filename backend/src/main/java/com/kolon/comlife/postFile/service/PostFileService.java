9cbc8cb109c9a07f8ec96ec75995c99713f2a952
Naram Kim a.k.a. Unknown <unknown@grotesq.com>
2018-02-05 13:57:54 +0900

    IOT-34 post로 받은 base64를 bytes로 변환하는 로직 추가. S3 업로드 구현 진행중.

--- backend/src/main/java/com/kolon/comlife/postFile/service/PostFileService.java
+++ backend/src/main/java/com/kolon/comlife/postFile/service/PostFileService.java
@@ -1,4 +1,13 @@
 package com.kolon.comlife.postFile.service;
 
+import com.kolon.comlife.postFile.model.PostFileInfo;
+
 public interface PostFileService {
+
+//    public PostFileInfo getPostFile( int id );
+//
+//    public PostFileInfo setPostFile( PostFileInfo postFile );
+//
+//    public void deletePostFile( int id );
+
 }
--- backend/src/main/java/com/kolon/comlife/postFile/web/PostFileController.java
+++ backend/src/main/java/com/kolon/comlife/postFile/web/PostFileController.java
@@ -1,4 +1,51 @@
 package com.kolon.comlife.postFile.web;
 
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.kolon.comlife.example.web.ExampleController;
+import com.kolon.comlife.postFile.model.PostFileInfo;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.xml.bind.DatatypeConverter;
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.util.HashMap;
+
+@RestController
+@RequestMapping("/postFile/*")
 public class PostFileController {
+    private static final Logger logger = LoggerFactory.getLogger(ExampleController.class);
+
+    @PostMapping(
+            value = "/",
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<PostFileInfo> setPostFile( @RequestBody HashMap<String, String> params ) {
+        String base64 = params.get( "file" );
+
+        String[] base64Components = base64.split(",");
+
+        if (base64Components.length != 2) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
+        }
+
+        String base64Data = base64Components[0];
+        String fileType = base64Data.substring(base64Data.indexOf('/') + 1, base64Data.indexOf(';'));
+        String base64Image = base64Components[1];
+        byte[] imageBytes = DatatypeConverter.parseBase64Binary(base64Image);
+
+        InputStream stream = new ByteArrayInputStream( imageBytes );
+        ObjectMetadata metadata = new ObjectMetadata();
+        metadata.setContentLength( imageBytes.length );
+        metadata.setContentType( "image/" + fileType );
+
+        logger.info( ">>>>>> base64: " + base64 );
+        return null;
+    }
 }
