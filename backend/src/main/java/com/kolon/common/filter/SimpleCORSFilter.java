3bba7791689a22b791b9a4916d547f1350bc7b11
Naram Kim a.k.a. Unknown <unknown@grotesq.com>
2018-02-18 14:25:19 +0900

    Access-Control-Allow-Methods 에 OPTIONS 추가. Access-Control-Allow-Headers 에서 모든 헤더 허용

--- backend/src/main/java/com/kolon/common/filter/SimpleCORSFilter.java
+++ backend/src/main/java/com/kolon/common/filter/SimpleCORSFilter.java
@@ -13,9 +13,9 @@ public class SimpleCORSFilter implements Filter {
     public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
         HttpServletResponse response = (HttpServletResponse) res;
         response.setHeader("Access-Control-Allow-Origin", "*");
-        response.setHeader("Access-Control-Allow-Methods", "POST, GET, DELETE, PUT");
+        response.setHeader("Access-Control-Allow-Methods", "POST, GET, DELETE, PUT, OPTIONS");
         response.setHeader("Access-Control-Max-Age", "3600");
-        response.setHeader("Access-Control-Allow-Headers", "x-requested-with, origin, content-type, accept");
+        response.setHeader("Access-Control-Allow-Headers", "*");
         chain.doFilter(req, res);
     }
 
