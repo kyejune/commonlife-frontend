0aaa362bf7b0169460e0b77fff84134800255f19
KyeJune Hong <hohoyaing@gmail.com>
2018-02-25 11:30:19 +0900

    (FE) 디바이스 테스트용 api주소 세팅

--- frontend/src/scripts/store.js
+++ frontend/src/scripts/store.js
@@ -6,48 +6,12 @@ if( window.location.hostname === 'localhost' || window.location.hostname === '12
     host = 'http://localhost:8080';
 
 // 디바이스 테스트용 주소
-if( window.location.protocol === 'file:' )// && window.location.pathname.includes('CoreSimulator') )
+if( window.location.protocol === 'file:' && window.location.pathname.includes('CoreSimulator') )
     host = 'http://192.168.0.100:8080';
 
-// function getUserIP(onNewIP) { //  onNewIp - your listener function for new IPs
-//     //compatibility for firefox and chrome
-//     var myPeerConnection = window.RTCPeerConnection || window.mozRTCPeerConnection || window.webkitRTCPeerConnection;
-//     var pc = new myPeerConnection({
-//             iceServers: []
-//         }),
-//         noop = function() {},
-//         localIPs = {},
-//         ipRegex = /([0-9]{1,3}(\.[0-9]{1,3}){3}|[a-f0-9]{1,4}(:[a-f0-9]{1,4}){7})/g,
-//         key;
-//
-//     function iterateIP(ip) {
-//         if (!localIPs[ip]) onNewIP(ip);
-//         localIPs[ip] = true;
-//     }
-//
-//     //create a bogus data channel
-//     pc.createDataChannel("");
-//
-//     // create offer and set local description
-//     pc.createOffer(function(sdp) {
-//         sdp.sdp.split('\n').forEach(function(line) {
-//             if (line.indexOf('candidate') < 0) return;
-//             line.match(ipRegex).forEach(iterateIP);
-//         });
-//
-//         pc.setLocalDescription(sdp, noop, noop);
-//     }, noop);
-//
-//     //listen for candidate events
-//     pc.onicecandidate = function(ice) {
-//         if (!ice || !ice.candidate || !ice.candidate.candidate || !ice.candidate.candidate.match(ipRegex)) return;
-//         ice.candidate.candidate.match(ipRegex).forEach(iterateIP);
-//     };
-// }
-//
-// getUserIP( ip => {
-//     console.log( 'ip:', ip );
-// });
+
+host = 'https://clback.cyville.net';
+
 
 const Store = observable({
 
