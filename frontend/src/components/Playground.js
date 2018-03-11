8cc694e84a44e8ef08eb00fe60d630841d2d1720
KyeJune Hong <june@KyeJunes-iMac.local>
2018-03-12 00:45:57 +0900

    FE) playground에 iot쪽 List정리중

--- frontend/src/components/Playground.js
+++ frontend/src/components/Playground.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import TimeScheduler from 'components/ui/TimeScheduler';
-import Store from "../scripts/store";
+import SampleSrc from 'images/io-t-icon-1@3x.png';
 
 
 class Playground extends Component{
@@ -12,6 +12,63 @@ class Playground extends Component{
             <h1>컴퍼넌트 테스트 페이지</h1>
 
 
+            <h3>Iot탭에 있는 세로 리스트들</h3>
+            <ul className="cl-iot-vertical-list cl-bg--white">
+                {/* Iot_기기제어 목록, 기기별 보기 */}
+                <li>
+                    <img className="cl__thumb" src={SampleSrc}/>
+                    <h4 className="cl__title">거실</h4>
+                    <div className="cl-next__button ml-auto"/>
+                </li>
+
+                {/* Iot_기기제어 상세 */}
+                <li>
+                    <img className="cl__thumb--rounded" src={SampleSrc}/>
+                    <div>
+                        <h4 className="cl__title">거실</h4>
+                        <p className="cl__desc">스위치 | OFF</p>
+                    </div>
+
+                    <div className="ml-auto">라디오 컴퍼넌트</div>
+                </li>
+            </ul>
+
+            <ul className="cl-iot-vertical-list cl-bg--dark">
+
+                {/* IoT_history 켜짐상태 */}
+                <li>
+                    <img className="cl__thumb--rounded" src={SampleSrc}/>
+                    <div>
+                        <h4 className="cl__title">거실조명</h4>
+                        <p className="cl__desc">조성우 | ON</p>
+                    </div>
+
+                    <div className="ml-auto cl__desc text-right">
+                        2017. 3.15<br/>
+                        AM 05:00
+                    </div>
+                </li>
+
+                {/* IoT_history 꺼짐상태 */}
+                <li>
+                    <img className="cl__thumb--rounded--off" src={SampleSrc}/>
+                    <div>
+                        <h4 className="cl__title">거실조명</h4>
+                        <p className="cl__desc">조성우 | OFF</p>
+                    </div>
+
+                    <div className="ml-auto cl__desc text-right">
+                        2017. 3.15<br/>
+                        AM 05:00
+                    </div>
+                </li>
+
+
+            </ul>
+
+
+
+            <h3>타입 스케쥴러</h3>
             <TimeScheduler // 24시간 기준으로 기입!!
                 maxWidth={3} // 최대시간, min-width는 0.5로 고정
                 min={10} // 예약가능한 시작 시간
---
+++ frontend/src/components/drawers/DraggableList.js
@@ -0,0 +1,16 @@
+import React,{Component} from 'react';
+
+
+
+class DraggableList extends Component{
+
+
+    render(){
+        return <div>
+            {...children()}
+        </div>
+    }
+}
+
+
+export default DraggableList
