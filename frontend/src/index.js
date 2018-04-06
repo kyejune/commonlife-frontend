fb6488e58e4db283dc7d4dbc6ed4e5436fda834b
Naram Kim a.k.a. Unknown <unknown@grotesq.com>
2018-04-06 15:15:04 +0900

    cordova.js 추가 스크립트 개선

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,7 +5,10 @@ import 'scripts/connect.js';
 import registerServiceWorker from './registerServiceWorker';
 
 if( window.location.protocol === 'file:' ) {
-    document.write('<script src="cordova.js"></script>');
+    const head = document.querySelector( 'head' );
+    const cordova = document.createElement( 'script' );
+    cordova.src = 'cordova.js';
+    head.appendChild( cordova );
 }
 
 ReactDOM.render(<App />, document.getElementById('root'));
