a7273ac70896a84651e43f73fa0d1adb19e78b8f
Naram Kim a.k.a. Unknown <unknown@grotesq.com>
2018-04-13 22:49:02 +0900

    콘솔에 api host 표시하도록 업데이트

--- frontend/src/scripts/store.js
+++ frontend/src/scripts/store.js
@@ -10,8 +10,9 @@ if( window.location.hostname === 'localhost' || window.location.hostname === '12
 if( window.location.protocol === 'file:' && window.location.pathname.includes('CoreSimulator') )
     host = 'http://192.168.0.100:8080';
 
+// 쿠키를 설정/삭제해 API 서버 주소를 고정시킬 수 있다.
 // Cookie.set( 'force-look-localhost', true );
-// Cookie.set( 'force-look-localhost', false );
+// Cookie.remove( 'force-look-localhost' );
 
 // 덮어쓰기
 // host = 'https://cl-stage.cyville.net'; // 클라 전달용
@@ -20,6 +21,8 @@ if( window.location.protocol === 'file:' && window.location.pathname.includes('C
 if( !Cookie.get( 'force-look-localhost' ) ) {
     host = 'https://clback.cyville.net'; // 테스트용
 }
+
+console.log( 'api host: ' + host );
 
 // 생성용 시나리오/자동화 객체
 /*
