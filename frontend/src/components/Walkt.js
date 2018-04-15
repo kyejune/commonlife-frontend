c8d60d081351054d75850d8880f5e8e99be37437
KyeJune Hong <hohoyaing@gmail.com>
2018-04-15 22:05:41 +0900

    FE) walkt페이지 레이아웃 기능 업데이트

--- frontend/src/components/Walkt.js
+++ frontend/src/components/Walkt.js
@@ -1,26 +1,59 @@
 import React, { Component } from 'react';
 import Swiper from 'react-id-swiper';
-import {Link} from "react-router-dom";
+import {Link, withRouter} from "react-router-dom";
    
 class Walkt extends Component {
 
     constructor(props) {
         super(props);
 
-        this.swiperParams = {
-            pagination: {
-                el: '.cl-pager',
-                type: 'fraction',
-            },
+        this.state = {
+            isEnd: false,
+        }
+    }
+
+    next=()=>{
+        if( this.state.isEnd ){
+            this.props.history.push('/community/feed');
+        }else{
+            this.swiper.swiper.slideNext();
         }
     }
 
 
     render() {
+
+        console.log( 'redner swiper');
+
+        const SELF = this;
+
+        const PARAMS = {
+            pagination: {
+                el: '.swiper-pagination',
+                type: 'fraction',
+            },
+
+            navigation: {
+                nextEl: '.swiper-button-next',
+            },
+            renderCustomNextButton: ()=>{
+                return <button className="cl-walkt-next__button" onClick={ this.next }>{ SELF.state.isEnd?"START":"NEXT" }</button>;
+            },
+
+            on:{
+                slideChange:function(){
+                    SELF.setState( { isEnd: this.activeIndex === 2 });
+                }
+            },
+
+        };
+
+
+
         return <div className="cl-full-page cl-walkt cl-bg--white">
 
 
-                <Swiper pagination={ { el:'.swiper-pagination', type:'fraction' } } >
+                <Swiper {...PARAMS } ref={ r => this.swiper = r }>
                   <div className="cl-walkt-page">
                       <h2>Community</h2>
                       <p>COMMON Life 지점간, 지점내 다양한 멤버와의<br/>네트워크를 통해 새로운 삶의 방식을 발견하세요.</p>
@@ -37,9 +70,11 @@ class Walkt extends Component {
                   </div>
                 </Swiper>
 
+                <Link to="/community/feed" className="cl-skip__button">SKIP</Link>
+
                 <footer className="fs-16 color-white">
                     <div className="cl-flex">
-                        <Link to="/community/feed" className="cl-bold ml-auto">SKIP</Link>
+                        {/*<Link to="/community/feed" className="cl-bold ml-auto cl-skip__button">SKIP</Link>*/}
                     </div>
                 </footer>
 
@@ -49,4 +84,4 @@ class Walkt extends Component {
 }
 
     
-export default Walkt;
+export default withRouter(Walkt);
