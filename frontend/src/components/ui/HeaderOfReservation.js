41c7285d2602c9e686e781729b3c2072b9f336d4
Naram Kim a.k.a. Unknown <unknown@grotesq.com>
2018-04-15 21:55:17 +0900

    [FE] 예약> 예약 대기 버튼 감춤

--- frontend/src/components/ui/HeaderOfReservation.js
+++ frontend/src/components/ui/HeaderOfReservation.js
@@ -36,8 +36,8 @@ class HeaderOfReservation extends Component {
 
 			<Link to={ '/reservation/history' } className="cl-reservation__link-recent-reserve cl-reservation__link-recent-reserve--ready">
 				<i><img src={recentReserveReady} alt="" height="20"/></i>
-				<span>예약대기</span>
-				<span className="cl-reservation__link-recent-reserve-count">0</span>
+				{/*<span>예약대기</span>*/}
+				{/*<span className="cl-reservation__link-recent-reserve-count">0</span>*/}
 			</Link>
 
 		</div>
