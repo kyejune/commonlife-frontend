5e32bf7a547c81c8f84709566006cfb547712a25
Young Kim <ykim.hello@gmail.com>
2018-05-19 12:18:51 +0900

    Admin) complex list Query 리펙토링

--- admin/src/main/java/com/kolon/comlife/admin/complexes/model/ComplexConst.java
+++ admin/src/main/java/com/kolon/comlife/admin/complexes/model/ComplexConst.java
@@ -6,10 +6,10 @@ import org.springframework.stereotype.Component;
 public class ComplexConst {
 
     // 아래 상수 값은 COMPLEX_GRP_TYPE_M 테이블의 CMPLX_GRP_TYPE_ID 값과 동일해야 합니다.
-    final static public int GRP_INITIAL_ID      = -1;        // 미분류: COMPLEX_GRP_TYPE_M.CMPLX_GRP_TYPE_ID = -1
+    final static public int GRP_INITIAL_ID      = -1;  // 미분류: COMPLEX_GRP_TYPE_M.CMPLX_GRP_TYPE_ID = -1
     final static public int GRP_PREPARATION_ID  = 0;   // 준비중: COMPLEX_GRP_TYPE_M.CMPLX_GRP_TYPE_ID = 0
-    final static public int GRP_MINGAN_ID       = 1;          // 민간: COMPLEX_GRP_TYPE_M.CMPLX_GRP_TYPE_ID = 1
-    final static public int GRP_GONGGONG_ID     = 2;        // 공공: COMPLEX_GRP_TYPE_M.CMPLX_GRP_TYPE_ID = 2
+    final static public int GRP_MINGAN_ID       = 1;   // 민간: COMPLEX_GRP_TYPE_M.CMPLX_GRP_TYPE_ID = 1
+    final static public int GRP_GONGGONG_ID     = 2;   // 공공: COMPLEX_GRP_TYPE_M.CMPLX_GRP_TYPE_ID = 2
 
     final static public String GRP_INITIAL      = "initial";
     final static public String GRP_PREPARATION  = "preparation";
